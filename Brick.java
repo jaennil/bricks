85b4808be7d68c982469d1678635abbb205af3d1
ssushnost <ssushnost@gmail.com>
2023-06-19 22:12:39 +0300

    proper rebound when ball collides edge of the brick

--- Brick.java
+++ Brick.java
@@ -55,11 +55,18 @@ abstract public class Brick extends Sprite {
 		Rectangle brickBottom = new Rectangle(brickBounds.x, brickBounds.y+brickBounds.height-1, brickBounds.width, 1);
 		Rectangle2D intersection = ballBounds.createIntersection(brickBounds);
 		Rectangle intersectionRect = intersection.getBounds();
-		if (intersectionRect.intersects(brickLeft) || intersectionRect.intersects(brickRight))
+		if (intersectionRect.intersects(brickLeft) && intersectionRect.intersects(brickTop))
+			ball.setDirection(90+45);
+		else if (intersectionRect.intersects(brickTop) && intersectionRect.intersects(brickRight))
+			ball.setDirection(45);
+		else if (intersectionRect.intersects(brickRight) && intersectionRect.intersects(brickBottom))
+			ball.setDirection(270+45);
+		else if (intersectionRect.intersects(brickBottom) && intersectionRect.intersects(brickLeft))
+			ball.setDirection(180 + 45);
+		else if (intersectionRect.intersects(brickLeft) || intersectionRect.intersects(brickRight))
 			ball.getVelocity().reverseX();
 		else if (intersectionRect.intersects(brickTop) || intersectionRect.intersects(brickBottom))
 			ball.getVelocity().reverseY();
-		ball.getVelocity().reverseY();
 	}
 
 	public void update() {
